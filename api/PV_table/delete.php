3c313e6acd06bca502dbfb990e86742a4f36188a
mmina001 <31961648+mmina001@users.noreply.github.com>
2018-12-03 01:17:17 +0200

    Back-end
    Deletes a PV System

---
+++ api/PV_table/delete.php
@@ -0,0 +1,34 @@
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: DELETE');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/PV_table.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate object
+  $pv_table = new PV_table($db);
+
+  // Get raw data
+  $data = json_decode(file_get_contents("php://input"));
+
+  $pv_table->ID = $data->ID;
+ 
+  // Delete PV System
+  if($pv_table->delete()){
+      echo json_encode(
+          array('message' => 'Post Deleted')
+      );
+  }else{
+    echo json_encode(
+        array('message' => 'Post Not Deleted')
+    );
+  }
+
+?>
