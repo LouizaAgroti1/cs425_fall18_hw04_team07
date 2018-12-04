8c4f15cd031fdad1ffa88fb357799a51f21b75c1
LouizaAgroti1 <31855678+LouizaAgroti1@users.noreply.github.com>
2018-12-04 20:51:25 +0200

    Login Operation final

--- login_db.php
+++ login_db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","root","","pv system");
+$conn = mysqli_connect("34.215.157.198","admin","1234","PV System");
 if(!$conn){
     die("Connection error: " . mysqli_connect_error());	
 }
